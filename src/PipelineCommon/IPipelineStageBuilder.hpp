8827f9ce6a13f8c6875e96e4ecb4c204eac1c3b4
Dmitriy <>
2019-08-10 13:31:01 -0700

    Changing interface

--- src/PipelineCommon/IPipelineStageBuilder.hpp
+++ src/PipelineCommon/IPipelineStageBuilder.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <vector>
 
+#include "IPipelineInterface.hpp"
 #include "IPipelineStage.hpp"
 
 namespace sc
@@ -13,9 +14,9 @@ class IPipelineStageBuilder
   public:
     virtual ~IPipelineStageBuilder();
 
-    virtual std::shared_ptr<std::vector<std::shared_ptr<IPipelineStage>>> buildPipeline() = 0;
+    virtual std::shared_ptr<std::vector<std::shared_ptr<IPipelineStage>>> createPipeline() = 0;
 
-    
+    virtual std::shared_ptr<IPipelineInterface> createPipelineInterface() = 0;
 };
 
 }  // namespace sc
