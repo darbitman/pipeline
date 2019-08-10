0d6653df932d9508f4954dd5122f3e81dec93579
Dmitriy <>
2019-08-10 08:44:08 -0700

    Adding methods to interface

--- src/PipelineCommon/IPipelineInterface.hpp
+++ src/PipelineCommon/IPipelineInterface.hpp
@@ -1,12 +1,24 @@
 #ifndef IPIPELINEINTERFACE_HPP
 #define IPIPELINEINTERFACE_HPP
 
+#include <memory>
+
+#include "BasePipelineData.hpp"
+
 namespace sc
 {
 class IPipelineInterface
 {
   public:
-    virtual ~IPipelineInterface();
+    virtual ~IPipelineInterface() = 0;
+
+    virtual void addNewDataToPipeline(std::shared_ptr<BasePipelineData> pPipelineData) = 0;
+
+    virtual std::shared_ptr<BasePipelineData> getOutputFromPipeline() = 0;
+
+    virtual bool doesNewResultExist() const = 0;
+
+    virtual size_t getNumberOfDataObjectsInPipeline() const = 0;
 };
 
 }  // namespace sc
