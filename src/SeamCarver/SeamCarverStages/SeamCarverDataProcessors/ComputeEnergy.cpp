607af3edf8ff2fa872b5db5632a14c7d83133485
Dmitriy <>
2019-08-14 20:26:26 -0700

    Added skeleton code

--- src/SeamCarver/SeamCarverStages/SeamCarverDataProcessors/ComputeEnergy.cpp
+++ src/SeamCarver/SeamCarverStages/SeamCarverDataProcessors/ComputeEnergy.cpp
@@ -1,12 +1,19 @@
 #include "ComputeEnergy.hpp"
 
 #include <memory>
+#include <opencv2/opencv.hpp>
+#include <thread>
+#include <vector>
 
 #include "VerticalSeamCarverData.hpp"
 
+using cv::Mat;
 using std::dynamic_pointer_cast;
 using std::make_shared;
+using std::ref;
 using std::shared_ptr;
+using std::thread;
+using std::vector;
 
 namespace sc
 {
@@ -15,6 +22,63 @@ ComputeEnergy::ComputeEnergy() {}
 ComputeEnergy::~ComputeEnergy() {}
 
 void ComputeEnergy::runSeamCarverProcessor(shared_ptr<VerticalSeamCarverData> pData)
+{
+    // TODO
+
+    // set local data members for the computation
+    numRows_ = pData->getNumberOfRows();
+    numColumns_ = pData->getNumberOfColumns();
+    bottomRow_ = pData->getBottomRowIndex();
+    rightColumn_ = pData->getRightColumnIndex();
+    numColorChannels_ = pData->getNumberOfColorChannels();
+    marginEnergy_ = pData->getEdgePixelEnergy();
+
+    calculatePixelEnergy(pData->getSavedImage(), )
+}
+
+void ComputeEnergy::calculatePixelEnergy(const shared_ptr<const Mat>& image,
+                                         vector<vector<double>>& outPixelEnergy)
+{
+    // if more columns, split calculation into 2 operations to calculate for every row
+    if (numColumns_ >= numRows_)
+    {
+        thread thread1(&ComputeEnergy::calculatePixelEnergyForEveryRow, this, ref(image),
+                       ref(outPixelEnergy), true);
+        thread thread2(&ComputeEnergy::calculatePixelEnergyForEveryRow, this, ref(image),
+                       ref(outPixelEnergy), false);
+        while (!thread1.joinable())
+            ;
+        thread1.join();
+        while (!thread2.joinable())
+            ;
+        thread2.join();
+    }
+    // otherwise, if more rows, split calculation into 2 operations to calculate for every column
+    else
+    {
+        thread thread1(&ComputeEnergy::calculatePixelEnergyForEveryColumn, this, ref(image),
+                       ref(outPixelEnergy), true);
+        thread thread2(&ComputeEnergy::calculatePixelEnergyForEveryColumn, this, ref(image),
+                       ref(outPixelEnergy), false);
+        while (!thread1.joinable())
+            ;
+        thread1.join();
+        while (!thread2.joinable())
+            ;
+        thread2.join();
+    }
+}
+
+void ComputeEnergy::calculatePixelEnergyForEveryRow(const shared_ptr<const Mat>& image,
+                                                    vector<vector<double>>& outPixelEnergy,
+                                                    bool bDoOddColumns)
+{
+    // TODO
+}
+
+void ComputeEnergy::calculatePixelEnergyForEveryColumn(const shared_ptr<const Mat>& image,
+                                                       vector<vector<double>>& outPixelEnergy,
+                                                       bool bDoOddRows)
 {
     // TODO
 }
--- src/SeamCarver/SeamCarverStages/SeamCarverDataProcessors/ComputeEnergy.hpp
+++ src/SeamCarver/SeamCarverStages/SeamCarverDataProcessors/ComputeEnergy.hpp
@@ -1,7 +1,9 @@
 #ifndef COMPUTEENERGY_HPP
 #define COMPUTEENERGY_HPP
 
+#include <vector>
 #include <memory>
+#include <opencv2/opencv.hpp>
 
 #include "ISeamCarverDataProcessor.hpp"
 #include "SeamCarverProcessorFactory.hpp"
@@ -25,6 +27,25 @@ class ComputeEnergy : public ISeamCarverDataProcessor
     ComputeEnergy& operator=(ComputeEnergy&&) = delete;
 
   private:
+    // image dimensions
+    size_t numRows_;
+    size_t numColumns_;
+    size_t bottomRow_;
+    size_t rightColumn_;
+    size_t numColorChannels_;
+    double marginEnergy_;
+
+    void calculatePixelEnergy(const std::shared_ptr<const cv::Mat>& image,
+                              std::vector<std::vector<double>>& outPixelEnergy);
+
+    void calculatePixelEnergyForEveryRow(const std::shared_ptr<const cv::Mat>& image,
+                                         std::vector<std::vector<double>>& outPixelEnergy,
+                                         bool bDoOddColumns);
+
+    void calculatePixelEnergyForEveryColumn(const std::shared_ptr<const cv::Mat>& image,
+                                            std::vector<std::vector<double>>& outPixelEnergy,
+                                            bool bDoOddRows);
+
     inline static const bool bRegistered_ =
         SeamCarverProcessorFactory::getFactoryInstance().registerNewStage(
             EPipelineStageId::STAGE_0, []() {
