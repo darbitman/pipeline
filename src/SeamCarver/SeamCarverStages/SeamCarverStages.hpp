d9ceeec43352e41098c26303262fe3e9d73319df
Dmitriy <>
2019-08-14 19:48:52 -0700

    Adding stages to header

--- src/SeamCarver/SeamCarverStages/SeamCarverStages.hpp
+++ src/SeamCarver/SeamCarverStages/SeamCarverStages.hpp
@@ -5,20 +5,20 @@
 #include "ComputeEnergyStage.hpp"
 #endif
 
-#if __has_include("")
-#include ""
+#if __has_include("CumulativePathEnergyStage.hpp")
+#include "CumulativePathEnergyStage.hpp"
 #endif
 
-#if __has_include("")
-#include ""
+#if __has_include("MergeChannelsStage.hpp")
+#include "MergeChannelsStage.hpp"
 #endif
 
-#if __has_include("")
-#include ""
+#if __has_include("SeamFinderStage.hpp")
+#include "SeamFinderStage.hpp"
 #endif
 
-#if __has_include("")
-#include ""
+#if __has_include("SeamRemoverStage.hpp")
+#include "SeamRemoverStage.hpp"
 #endif
 
 #endif
