f074949764e7318be4fecc123577cf1950cd2cb0
Dmitriy A <d.arbitman@gmail.com>
2019-10-19 15:49:37 -0700

    Checking for nullptr

--- src/Common/Matrix.hpp
+++ src/Common/Matrix.hpp
@@ -20,6 +20,20 @@ class Matrix : public IArray2D<T>
     {
     }
 
+    Matrix(Matrix&& other) noexcept
+        : maxRows_(other.maxRows_),
+          maxElements_(other.maxElements_),
+          numRows_(other.numRows_),
+          numColumns_(other.numColumns_),
+          p2DArray_(other.p2DArray_)
+    {
+        other.maxRows_ = 0;
+        other.maxElements_ = 0;
+        other.numRows_ = 0;
+        other.numColumns_ = 0;
+        other.p2DArray_ = nullptr;
+    }
+
     virtual ~Matrix() { DestructAndFreeCurrentMemory(); }
 
     virtual void resize(size_t numRows, size_t numColumns) noexcept override
@@ -171,7 +185,6 @@ class Matrix : public IArray2D<T>
     }
 
     Matrix(const Matrix&) = delete;
-    Matrix(Matrix&&) = delete;
     Matrix& operator=(const Matrix&) = delete;
     Matrix& operator=(Matrix&&) = delete;
 
@@ -185,17 +198,11 @@ class Matrix : public IArray2D<T>
 
     bool VerifyColumnAccess(const size_t column) const noexcept { return column < numColumns_; }
 
-    void DestructAndFreeCurrentMemory()
+    void DestructAndFreeCurrentMemory() noexcept
     {
         InvokeAllObjectDestructors();
 
-        // free the memory allocated for contigious memory (this is where the objects are stored)
-        ::operator delete(p2DArray_[0]);
-
-        // free the memory allocated for storing pointers to the contigous memory
-        ::operator delete(p2DArray_);
-
-        p2DArray_ = nullptr;
+        DeallocateMemory();
     }
 
     T** AllocateMatrix(size_t numRows, size_t numColumns)
@@ -221,24 +228,45 @@ class Matrix : public IArray2D<T>
     /// @param numColumns The number of columns in each row
     void UpdateArrayOfRowPointers(T** pArrayOfPointers, size_t numRows, size_t numColumns)
     {
-        // Update each pointer in the array to point to the next 'row' in the contigous array
-        for (size_t row = 1; row < numRows; ++row)
+        if (pArrayOfPointers != nullptr)
         {
-            // current pointer points to where the previous pointer points to plus an offset
-            // determined by the number of columns (ie points to the next 'row' in the contiguous
-            // array);
-            pArrayOfPointers[row] = pArrayOfPointers[row - 1] + numColumns;
+            // Update each pointer in the array to point to the next 'row' in the contigous array
+            for (size_t row = 1; row < numRows; ++row)
+            {
+                // current pointer points to where the previous pointer points to plus an offset
+                // determined by the number of columns (ie points to the next 'row' in the
+                // contiguous array);
+                pArrayOfPointers[row] = pArrayOfPointers[row - 1] + numColumns;
+            }
         }
     }
 
     /// @brief Invokes the destructors of all the elements in the contiguous array
-    void InvokeAllObjectDestructors()
+    void InvokeAllObjectDestructors() noexcept
     {
-        // explicitly need to call destructor since placement new was used for elements in the
-        // contiguous array
-        for (size_t i = 0; i < maxElements_; ++i)
+        if (p2DArray_ != nullptr)
         {
-            p2DArray_[0][i].~T();
+            // explicitly need to call destructor since placement new was used for elements in the
+            // contiguous array
+            for (size_t i = 0; i < maxElements_; ++i)
+            {
+                p2DArray_[0][i].~T();
+            }
+        }
+    }
+
+    void DeallocateMemory() noexcept
+    {
+        if (p2DArray_ != nullptr)
+        {
+            // free the memory allocated for contigious memory (this is where the objects are
+            // stored)
+            ::operator delete(p2DArray_[0]);
+
+            // free the memory allocated for storing pointers to the contigous memory
+            ::operator delete(p2DArray_);
+
+            p2DArray_ = nullptr;
         }
     }
 
