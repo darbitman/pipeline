39b4305dcea58dd48eea125ed827e68c89156c3a
Dmitriy A <d.arbitman@gmail.com>
2019-10-19 11:32:58 -0700

    Refactor

--- src/Common/Matrix.hpp
+++ src/Common/Matrix.hpp
@@ -30,17 +30,15 @@ class Matrix : public IArray2D<T>
         // actual objects is limited to maxElements_
         if (numRows <= maxRows_ && (numRows * numColumns) <= maxElements_)
         {
-            // update array of pointers to point to new 'rows' in the contiguous array, where each
-            // row has numColumns elements
-            for (size_t row = 1; row < numRows; ++row)
-            {
-                p2DArray_[row] = p2DArray_[row - 1] + numColumns_;
-            }
+            // Index 0 of p2DArray_ still points to the same contiguous array
+            UpdateArrayOfRowPointers(p2DArray_, numRows, numColumns);
 
             // set new dimensions
             numRows_ = numRows;
             numColumns_ = numColumns;
             numElements_ = numRows * numColumns;
+
+            // Don't update the max variables because arrays have not changed
         }
 
         // If more rows are needed, but total number of elements doesn't increase, then only the
@@ -51,22 +49,18 @@ class Matrix : public IArray2D<T>
             // preserve pointer to contiguous array
             T* pContiguousArray = p2DArray_[0];
 
-            // free the memory allocated for storing pointers to the contigous memory
+            // free the memory allocated for storing pointers to the contiguous array
             ::operator delete(p2DArray_);
 
-            // allocate new memory that will hold pointers to each 'row' in the contiguous
+            // allocate new array that will hold pointers to each 'row' in the contiguous
             // array
             p2DArray_ = reinterpret_cast<T**>((::operator new(numRows * sizeof(T*))));
 
-            // Set the pointer to the contiguous array
+            // Set index 0 of the array of pointers to point to the beginning of the contiguous
+            // array
             p2DArray_[0] = pContiguousArray;
 
-            // update array of pointers to point to new 'rows' in the contiguous array, where each
-            // row has numColumns elements
-            for (size_t row = 1; row < numRows; ++row)
-            {
-                p2DArray_[row] = p2DArray_[row - 1] + numColumns_;
-            }
+            UpdateArrayOfRowPointers(p2DArray_, numRows, numColumns);
 
             // update dimensions
             maxRows_ = numRows;
@@ -81,29 +75,19 @@ class Matrix : public IArray2D<T>
         // will need to be deleted and reallocated
         else if (numRows <= maxRows_ && (numRows * numColumns) > maxElements_)
         {
-            // Delete the objects explicitly by calling the destructor since placement new was used
-            // for elements in the contiguous array
-            for (size_t i = 0; i < maxElements_; ++i)
-            {
-                p2DArray_[0][i].~T();
-            }
+            InvokeAllObjectDestructors();
 
             // free the memory allocated for contiguous array
             ::operator delete(p2DArray_[0]);
 
+            // since maxElements has been exceed, the value can be updated
             // allocate new contiguous memory for storing objects
-            numElements_ = numRows * numColumns;
-            p2DArray_[0] = reinterpret_cast<T*>(::operator new(numElements_ * sizeof(T)));
+            maxElements_ = numElements_ = numRows * numColumns;
+            p2DArray_[0] = reinterpret_cast<T*>(::operator new(maxElements_ * sizeof(T)));
 
-            // update array of pointers to point to new 'rows' in the contiguous array, where each
-            // row has numColumns elements
-            for (size_t row = 1; row < numRows; ++row)
-            {
-                p2DArray_[row] = p2DArray_[row - 1] + numColumns_;
-            }
+            UpdateArrayOfRowPointers(p2DArray_, numRows, numColumns);
 
             // update dimensions
-            maxElements_ = numElements_;
             numRows_ = numRows;
             numColumns_ = numColumns;
         }
@@ -203,12 +187,7 @@ class Matrix : public IArray2D<T>
 
     void DestructAndFreeCurrentMemory()
     {
-        // explicitly need to call destructor since placement new was used for elements in the
-        // linear array
-        for (size_t i = 0; i < maxElements_; ++i)
-        {
-            p2DArray_[0][i].~T();
-        }
+        InvokeAllObjectDestructors();
 
         // free the memory allocated for contigious memory (this is where the objects are stored)
         ::operator delete(p2DArray_[0]);
@@ -230,15 +209,37 @@ class Matrix : public IArray2D<T>
         // create contiguous array
         pArrayOfPointers[0] = reinterpret_cast<T*>(::operator new(numElements * sizeof(T)));
 
+        UpdateArrayOfRowPointers(pArrayOfPointers, numRows, numColumns);
+
+        return pArrayOfPointers;
+    }
+
+    /// @param pArrayOfPointers Points to the array of pointers that point to each 'row' in the
+    /// contiguous array. Index 0 of this array must be initialized to point to the beginning of the
+    /// contiguous array prior to calling this method.
+    /// @param numRows The number of rows
+    /// @param numColumns The number of columns in each row
+    void UpdateArrayOfRowPointers(T** pArrayOfPointers, size_t numRows, size_t numColumns)
+    {
         // Update each pointer in the array to point to the next 'row' in the contigous array
         for (size_t row = 1; row < numRows; ++row)
         {
-            // current pointer points to where the previous pointer points to plus an offset of the
-            // number of columns (ie points to the next row)
+            // current pointer points to where the previous pointer points to plus an offset
+            // determined by the number of columns (ie points to the next 'row' in the contiguous
+            // array);
             pArrayOfPointers[row] = pArrayOfPointers[row - 1] + numColumns;
         }
+    }
 
-        return pArrayOfPointers;
+    /// @brief Invokes the destructors of all the elements in the contiguous array
+    void InvokeAllObjectDestructors()
+    {
+        // explicitly need to call destructor since placement new was used for elements in the
+        // contiguous array
+        for (size_t i = 0; i < maxElements_; ++i)
+        {
+            p2DArray_[0][i].~T();
+        }
     }
 
     /// Maximum number of rows in array
