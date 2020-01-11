7806cf901638cf320601ebc32f0039171ad19763
Dmitriy A <d.arbitman@gmail.com>
2020-01-11 11:41:35 -0800

    Update benchmark

--- src/Common/bench/Array2DBench.cpp
+++ src/Common/bench/Array2DBench.cpp
@@ -3,12 +3,13 @@
 #include <cstdint>
 #include <vector>
 
+#include "Matrix.hpp"
 #include "Vector2D.hpp"
 
 static void BM_Vector2D(benchmark::State& state)
 {
-    constexpr size_t width{1920};
-    constexpr size_t height{1080};
+    size_t width = state.range(0);
+    size_t height = state.range(1);
 
     pipeline::Vector2D<uint32_t> vec(height, width);
 
@@ -21,15 +22,16 @@ static void BM_Vector2D(benchmark::State& state)
                 vec.at(row, col) = rand();
             }
         }
+        benchmark::ClobberMemory();
     }
 }
 
-BENCHMARK(BM_Vector2D);
+BENCHMARK(BM_Vector2D)->Args({1920, 1080});
 
 static void BM_StdVector2D(benchmark::State& state)
 {
-    constexpr size_t width{1920};
-    constexpr size_t height{1080};
+    size_t width = state.range(0);
+    size_t height = state.range(1);
 
     std::vector<std::vector<uint32_t>> vec;
     vec.resize(height);
@@ -40,16 +42,39 @@ static void BM_StdVector2D(benchmark::State& state)
 
     for (auto _ : state)
     {
-        for (size_t row = 0; row < height; ++row)
+        for (size_t col = 0; col < width; ++col)
         {
-            for (size_t col = 0; col < width; ++col)
+            for (size_t row = 0; row < height; ++row)
             {
                 vec[row][col] = rand();
             }
         }
+        benchmark::ClobberMemory();
     }
 }
 
-BENCHMARK(BM_Vector2D);
+BENCHMARK(BM_StdVector2D)->Args({1920, 1080});
+
+// static void BM_Matrix(benchmark::State& state)
+// {
+//     size_t width = state.range(0);
+//     size_t height = state.range(1);
+
+//     pipeline::Matrix<uint32_t> vec(width, height);
+
+//     for (auto _ : state)
+//     {
+//         for (size_t row = 0; row < height; ++row)
+//         {
+//             for (size_t col = 0; col < width; ++col)
+//             {
+//                 vec.at(row, col) = rand();
+//             }
+//         }
+//         benchmark::ClobberMemory();
+//     }
+// }
+
+// BENCHMARK(BM_Matrix)->Args({1920, 1080});
 
 BENCHMARK_MAIN();
