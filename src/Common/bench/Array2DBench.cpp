aa4ddffd5d768e3edff0a7adab73e9a3800a4198
Dmitriy A <d.arbitman@gmail.com>
2020-01-11 11:20:28 -0800

    Update bench

--- src/Common/bench/Array2DBench.cpp
+++ src/Common/bench/Array2DBench.cpp
@@ -1,36 +1,55 @@
 #include <benchmark/benchmark.h>
+#include <stdlib.h>
+#include <cstdint>
+#include <vector>
 
 #include "Vector2D.hpp"
 
+static void BM_Vector2D(benchmark::State& state)
+{
+    constexpr size_t width{1920};
+    constexpr size_t height{1080};
+
+    pipeline::Vector2D<uint32_t> vec(height, width);
+
+    for (auto _ : state)
+    {
+        for (size_t row = 0; row < height; ++row)
+        {
+            for (size_t col = 0; col < width; ++col)
+            {
+                vec.at(row, col) = rand();
+            }
+        }
+    }
+}
+
+BENCHMARK(BM_Vector2D);
+
+static void BM_StdVector2D(benchmark::State& state)
+{
+    constexpr size_t width{1920};
+    constexpr size_t height{1080};
+
+    std::vector<std::vector<uint32_t>> vec;
+    vec.resize(height);
+    for (size_t row = 0; row < height; ++row)
+    {
+        vec.at(row).resize(width);
+    }
+
+    for (auto _ : state)
+    {
+        for (size_t row = 0; row < height; ++row)
+        {
+            for (size_t col = 0; col < width; ++col)
+            {
+                vec[row][col] = rand();
+            }
+        }
+    }
+}
+
+BENCHMARK(BM_Vector2D);
+
 BENCHMARK_MAIN();
-
-// #include <benchmark/benchmark.h>
-
-// #include "FixedAllocator.hpp"
-
-// static void BM_AllocateUsingFixedAllocator(benchmark::State& state)
-// {
-//     FixedAllocator allocator(size_t(state.range(0)));
-
-//     for (auto _ : state)
-//     {
-//         auto* p_memory = allocator.Allocate();
-//         benchmark::DoNotOptimize(p_memory);
-//     }
-// }
-
-// BENCHMARK(BM_AllocateUsingFixedAllocator)->RangeMultiplier(2)->Range(4, 64);
-
-// static void BM_AllocateUsingOpNew(benchmark::State& state)
-// {
-//     size_t object_size = size_t(state.range(0));
-//     for (auto _ : state)
-//     {
-//         auto* p_memory = ::operator new(object_size);
-//         benchmark::DoNotOptimize(p_memory);
-//     }
-// }
-
-// BENCHMARK(BM_AllocateUsingOpNew)->RangeMultiplier(2)->Range(4, 64);
-
-// BENCHMARK_MAIN();
